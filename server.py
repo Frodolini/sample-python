53da524081757db0d444dee9d95456871d3bf3f7
Frodolini <70431079+Frodolini@users.noreply.github.com>
2020-10-07 22:50:51 +0200

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
+        msg = 'Hei på deg fremmede på internettet! Du førespurde %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
